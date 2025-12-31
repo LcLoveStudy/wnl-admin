37979e10f22238b30e315f10ad6cfb2be97173da
lichang <dotb116393@163.com>
2025-12-31 17:00:45 +0800

    :bug: 登录页-修复背景不切换的问题

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,10 @@
   <div class="h-full grid tablet:grid-cols-[2fr_1fr]">
     <!-- 左边背景 -->
     <div
-      class="primary_box bg-img-light dark:bg-img-dark phone:hidden tablet:flex items-center justify-center"
+      :class="[
+        'primary_box phone:hidden tablet:flex items-center justify-center',
+        theme === 'dark' ? 'bg-img-dark' : 'bg-img-light',
+      ]"
     >
       <img class="w-1/3 select-none primary_img" :src="PrimaryImg" />
     </div>
@@ -14,6 +17,10 @@
 <script setup lang="ts">
 import PrimaryImg from '@/assets/images/login/primary.png'
 import { FormSide } from './components'
+import { useCustomStore } from '@/stores'
+import { storeToRefs } from 'pinia'
+
+const { theme } = storeToRefs(useCustomStore())
 </script>
 
 <style lang="less" scoped>
@@ -32,5 +39,29 @@ import { FormSide } from './components'
   .primary_img {
     animation: riseAndFall 2s infinite;
   }
+}
+</style>
+
+<style lang="less" scoped>
+.bg-img-light {
+  background-image: radial-gradient(
+    circle at center,
+    #f8fafc 0%,
+    #f8fafc 25%,
+    #dbeafe 50%,
+    #f8fafc 75%,
+    #f8fafc 100%
+  );
+}
+
+.bg-img-dark {
+  background-image: radial-gradient(
+    circle at center,
+    black 0%,
+    black 25%,
+    #1e2a47 50%,
+    black 75%,
+    black 100%
+  );
 }
 </style>

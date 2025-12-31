5e792b07d3d5484e7879bff8ec7d96d1fe1f732c
lichang <dotb116393@163.com>
2025-12-31 17:03:00 +0800

    :art: 主题切换-修改icon判断方法

--- src/components/theme-checkout/index.vue
+++ src/components/theme-checkout/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="flex items-center cursor-pointer theme-btn">
-    <sun-icon @click="checkoutTheme" v-if="theme === 'dark'" />
-    <moon-icon @click="checkoutTheme" v-else />
+    <component :is="iconComponent" @click="checkoutTheme" />
   </div>
 </template>
 
 <script setup lang="ts">
-  import SunIcon from '@/components/svg/sun.vue'
-  import MoonIcon from '@/components/svg/moon.vue'
-  import { useCustomStore } from '@/stores'
-  import { storeToRefs } from 'pinia'
-  const { theme } = storeToRefs(useCustomStore())
-  const { checkoutTheme } = useCustomStore()
+import SunIcon from '@/components/svg/sun.vue'
+import MoonIcon from '@/components/svg/moon.vue'
+import { useCustomStore } from '@/stores'
+import { storeToRefs } from 'pinia'
+const { theme } = storeToRefs(useCustomStore())
+const { checkoutTheme } = useCustomStore()
+const iconComponent = computed(() => {
+  return theme.value === 'dark' ? SunIcon : MoonIcon
+})
 </script>

917f1bc2f703b2ac264b8c611b8d4819ab5f1f7f
lichang <dotb116393@163.com>
2026-03-16 16:11:48 +0800

    :hammer: 修改路由模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { setupRouterGuards, setupTabGuards } from './guards'
 import { rootRoute, staticRoutes } from '@/router/config/staticRoutes'
 import { formatRoutes } from '@/router/utils'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [...formatRoutes(staticRoutes), rootRoute],
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wln-admin/',
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
